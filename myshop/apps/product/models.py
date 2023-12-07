020283a7909d378a84bfdac33a530138e0f5c1fb
Artem <art.pzt@gmail.com>
2023-12-07 21:04:17 +0200

    [lesson 8] Django tests

--- myshop/apps/product/models.py
+++ myshop/apps/product/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=256)
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now=True, auto_created=True)
     description = models.TextField(null=True, blank=True)
 
     def __str__(self):
