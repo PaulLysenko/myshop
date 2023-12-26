aedbb6fb49c5df14e8cd79a508c9fc955274b322
djooseo <sofiakorneva62@gmail.com>
2023-12-26 16:38:27 +0200

    hw4 commit

--- myshop/apps/account/models.py
+++ myshop/apps/account/models.py
@@ -1,6 +1,10 @@
 import uuid
+
+from django.conf import settings
+from django.core import mail
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.loader import render_to_string
 
 
 class RegTry(models.Model):
@@ -9,6 +13,25 @@ class RegTry(models.Model):
     otc = models.UUIDField(default=uuid.uuid4, editable=False, db_index=True)
     c_time = models.DateTimeField(auto_now=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+
+    def send_reg_mail(self):
+        context = {
+            'link': f'http://{settings.HOST_NAME}/registration/{self.otc}',
+            # more data here for customisation email?
+        }
+        mail_data = {
+            'subject': 'Please verify your account',
+            'message': 'Please verify your account',
+            'from_email': settings.EMAIL_HOST_USER,
+            'recipient_list': [self.email],
+            'html_message': render_to_string('registration_confirm.html', context=context),
+        }
+
+        try:
+            mail.send_mail(**mail_data)  # into celery task
+            print(f'success {context["link"]}')
+        except Exception as exception:  # pylint: disable=W0703
+            print(f'[SENDMAIL] EXCEPTION {exception}')
 
     def __str__(self):
         return f"{self.email} at {self.c_time.strftime('%d-%m-%Y %H:%M:%S')}"
--- myshop/apps/account/views.py
+++ myshop/apps/account/views.py
@@ -1,15 +1,13 @@
 from gettext import gettext as _
 
 from django.contrib.auth.models import User
-from django.conf import settings
-from django.core import mail
 from django.shortcuts import render, redirect, get_object_or_404
-from django.template.loader import render_to_string
 from django.urls import reverse
 from django.views import View
 
 from apps.account.models import RegTry
 from apps.account.forms import RegTryForm, ValidateRegTryForm
+from apps.account.utilities import user_creation
 
 
 class RegTryView(View):
@@ -36,29 +34,7 @@ class RegTryView(View):
             return render(request, 'invalid_data.html', {'data': 'Email is not valid.'}, status=400)
 
         reg_try = RegTry.objects.create(email=email_value)
-
-        # todo: into function send_reg_mail->
-            # todo: into function -->
-        context = {
-            'link': f'http://{settings.HOST_NAME}/registration/{reg_try.otc}',
-            # more data here for customisation email?
-        }
-        mail_data = {
-            'subject': 'Please verify your account',
-            'message': 'Please verify your account',
-            'from_email': settings.EMAIL_HOST_USER,
-            'recipient_list': [reg_try.email],
-            'html_message': render_to_string('registration_confirm.html', context=context),
-        }
-            # todo: into function <--
-
-        try:
-            mail.send_mail(**mail_data)  # into celery task
-            print(f'success {context["link"]}')
-        except Exception as exception:  # pylint: disable=W0703
-            print(f'[SENDMAIL] EXCEPTION {exception}')
-
-        # todo: into function send_reg_mail <-
+        reg_try.send_reg_mail()
 
         return redirect(reverse('home'))
 
@@ -91,16 +67,5 @@ class ValidateRegTryView(View):
 
             return render(request, self.template_name, context=context)
 
-        # todo:  into function ->
-        user = User.objects.create_user(
-            username=f"{form.cleaned_data['first_name']} {form.cleaned_data['last_name']}",
-            email=reg_try.email,
-            password=form.cleaned_data['password'],
-            first_name=form.cleaned_data['first_name'],
-            last_name=form.cleaned_data['last_name'],
-        )
-        reg_try.user = user
-        reg_try.save()
-        # todo:  into function <-
-
+        user_creation(form, reg_try)
         return redirect(reverse('home'))
